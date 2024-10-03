387f1c4a11210ffec7beb90a48d7b4691172d4bf
UBC Student <egegures@gmail.com>
2024-10-02 18:02:37 -0700

    update .gitignore

---
+++ TodoList/TaskHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskHistoryView.swift
+//  TodoList
+//
+//  Created by Ege Gures on 2024-08-27.
+//
+
+import SwiftUI
+
+struct TaskHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskHistoryView()
+}
